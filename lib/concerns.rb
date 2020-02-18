79e7f37ceed8027e4885592fb93bc158b0d4b042
Meghan Flynn <mflynn333@gmail.com>
2020-02-18 19:27:57 +0000

    Automatically backed up by Learn

--- lib/concerns.rb
+++ lib/concerns.rb
@@ -1,13 +1,11 @@
 module Concerns::Findable
 
-  def find_by_name(name)
-     all.find do |song_detect|
-       song_detect.name == name
-     end
+  def self.find_by_name(song)
+    self.all.detect { |s| s.name == song }
   end
-
-  def find_or_create_by_name(name) #
-      self.find_by_name(name) || self.create(name)
+  
+  def self.find_or_create_by_name(song)
+    find_by_name(song) || create(song)
   end
 
 end 
