bb949352466f0c47189c0ba70f6feda719addbec
Meghan Flynn <mflynn333@gmail.com>
2020-02-18 20:00:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -58,6 +58,19 @@ class MusicLibraryController
       puts "#{index}. #{genre.name}"
     end
   end
+  
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    input = gets.chomp
+    if artist = Artist.find_by_name(input)
+      songs_sorted_by_name = artist.songs.sort_by do |song|
+        song.name
+      end
+      songs_sorted_by_name.each.with_index(1) do |song,index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
 
   def play_song
     puts "Which song number would you like to play?"
