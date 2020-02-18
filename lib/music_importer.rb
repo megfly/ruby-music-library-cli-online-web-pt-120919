ae21c9923d2a174735ef9d6844b3dc00eb1bb211
Meghan Flynn <mflynn333@gmail.com>
2020-02-18 19:41:47 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,5 +9,9 @@ class MusicImporter
   def files 
     Dir.entries(path).select{ |file| file.end_with?(".mp3") }
   end 
+  
+  def import
+    self.files.each{ |filename| Song.create_from_filename(filename)}
+  end
 
 end 
