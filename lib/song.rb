90e66dab642d9c9cb064e169696967227c9a3cbd
Meghan Flynn <mflynn333@gmail.com>
2020-02-18 19:14:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,12 @@ class Song
   end 
   
   def self.find_by_name(name)
-    
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end 
+    end
+  end 
   end 
   
   def find_or_create_by_name 
