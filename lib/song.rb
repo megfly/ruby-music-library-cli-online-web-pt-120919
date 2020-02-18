3df384d41f23b6204c8baaa4d171eb1e7516d68d
Meghan Flynn <mflynn333@gmail.com>
2020-02-18 19:40:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,10 +52,8 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    title = filename.split(" - ")
-    artist = Artist.find_or_create_by_name(title[0])
-    genre = Genre.find_or_create_by_name(title[2][0..-5])
-    create(title[1], artist, genre)
-  end 
+    artist_name, song_name, genre = filename.gsub(".mp3","").split(" - ")
+    new(song_name,Artist.find_or_create_by_name(artist_name),Genre.find_or_create_by_name(genre))
+  end
 
 end 
