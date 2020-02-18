a6805978636f31a223feb574a7ab30d24468948a
Meghan Flynn <mflynn333@gmail.com>
2020-02-18 20:02:52 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -76,11 +76,11 @@ class MusicLibraryController
     puts "Please enter the name of a genre:"
     input = gets.chomp
     if genre = Genre.find_by_name(input) 
-      songs_sorted_by_name = genre.songs.sort_by do |song|
-        song.name
+      songs_sorted_by_name = genre.songs.sort_by do |s|
+        s.name
       end
-      songs_sorted_by_name.each.with_index(1) do |song,index|
-        puts "#{index}. #{song.artist.name} - #{song.name}"
+      songs_sorted_by_name.each.with_index(1) do |s,index|
+        puts "#{index}. #{s.artist.name} - #{s.name}"
       end
     end
   end
